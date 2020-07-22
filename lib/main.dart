c9d475cd62be582d7db4719ed75452fbfdf19974
Matheus Mayer <matheusmayerduarte@gmail.com>
2020-07-21 21:16:42 -0300

    Adicionando features de adicao e remocao do item

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'models/item.dart';
 
 void main() {
   runApp(App());
@@ -19,17 +20,93 @@ class App extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+
+  HomePage() {
+    items = [
+      Item(title: "Item 1", done: false),
+      Item(title: "Item 2", done: true),
+      Item(title: "Item 3", done: false)
+    ];
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var newTaskController = TextEditingController();
+
+  void add() {
+    setState(() {
+      if (newTaskController.text.isNotEmpty) {
+        widget.items.add(
+          Item(
+            title: newTaskController.text,
+            done: false,
+          ),
+        );
+      }
+
+      newTaskController.clear();
+    });
+  }
+
+  void remove(int index) {
+    setState(() {
+      widget.items.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Todo List'),
-      ),
-      body: Container(
-        child: Center(
-          child: Text('Olar mundo'),
+        title: TextFormField(
+          controller: newTaskController,
+          keyboardType: TextInputType.text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+          ),
+          decoration: InputDecoration(
+            labelText: 'Nova Tarefa',
+            labelStyle: TextStyle(
+              color: Colors.white,
+            ),
+          ),
         ),
+      ),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (BuildContext context, int index) {
+          final item = widget.items[index];
+
+          return Dismissible(
+            child: CheckboxListTile(
+              title: Text(item.title),
+              value: item.done,
+              onChanged: (bool value) {
+                setState(() {
+                  item.done = value;
+                });
+              },
+            ),
+            key: Key(item.title),
+            background: Container(
+              color: Colors.red.withOpacity(0.2),
+            ),
+            onDismissed: (direction) {
+              remove(index);
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: add,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blue,
       ),
     );
   }
